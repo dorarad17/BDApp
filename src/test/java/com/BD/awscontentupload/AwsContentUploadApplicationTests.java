2113bc091c5a9e7071cc5aa0029df521ff3e73ad
doraradosevic-bbc <dora.radosevic@bbc.co.uk>
2020-09-25 18:06:23 +0100

    initial commit

---
+++ src/test/java/com/BD/awscontentupload/AwsContentUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BD.awscontentupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsContentUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
