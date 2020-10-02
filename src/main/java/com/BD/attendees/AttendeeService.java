97179ab32c4c98eef5776b2e8b7a9dbf6e47c026
doraradosevic-bbc <dora.radosevic@bbc.co.uk>
2020-10-02 16:39:20 +0100

    finished http methods and put in dummy config in application.properties

--- src/main/java/com/BD/attendees/AttendeeService.java
+++ src/main/java/com/BD/attendees/AttendeeService.java
@@ -2,16 +2,17 @@ package com.BD.attendees;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class AttendeeService {
 
-    private List<Attendee> attendees = Arrays.asList(
+    private List<Attendee> attendees = new ArrayList<>(Arrays.asList(
                 new Attendee("0001","Liam","Shove"),
                 new Attendee("0002","Dora","Radosevic"),
-                new Attendee("0003","Gary","Z"));
+                new Attendee("0003","Gary","Z")));
 
     public List<Attendee> getAllAttendees(){
         return attendees;
@@ -19,5 +20,22 @@ public class AttendeeService {
 
     public Attendee getAttendee(String id){
         return attendees.stream().filter(a -> a.getId().equals(id)).findAny().get();
+    }
+
+    public void addAttendee(Attendee attendee) {
+        attendees.add(attendee);
+    }
+
+    public void updateAttendee(String id, Attendee attendee) {
+        for(Attendee a : attendees){
+            if (a.getId().equals(id)) {
+                attendees.set(attendees.indexOf(a),attendee);
+                return;
+            }
+        }
+    }
+
+    public void deleteAttendee(String id) {
+        attendees.removeIf(a -> a.getId().equals(id));
     }
 }
--- src/main/java/com/BD/controller/AttendeeController.java
+++ src/main/java/com/BD/controller/AttendeeController.java
@@ -3,9 +3,7 @@ package com.BD.controller;
 import com.BD.attendees.Attendee;
 import com.BD.attendees.AttendeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,5 +22,20 @@ public class AttendeeController {
     @RequestMapping("/attendees/{id}")
     public Attendee getAttendee(@PathVariable String id) {
         return attendeeService.getAttendee(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/attendees")
+    public void addAttendee(@RequestBody Attendee attendee){
+        attendeeService.addAttendee(attendee);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/attendees/{id}")
+    public void updateAttendee(@RequestBody Attendee attendee,@PathVariable String id){
+        attendeeService.updateAttendee(id,attendee);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/attendees/{id}")
+    public void deleteAttendee(@PathVariable String id){
+        attendeeService.deleteAttendee(id);
     }
 }
